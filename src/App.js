4119b024e7d20b55e356753328d93e29e8d9d632
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-04-08 05:11:22 +0700

    + folder structure and init routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import Users from './users/pages/Users'
+import NewPlace from './places/pages/NewPlace'
 
-function App() {
-  return <h1>Let's start!</h1>;
+const  App = () => {
+  	return (
+    	<Router>
+			<Switch>
+				<Route path="/" component={Users} exact />
+				<Route path="/places/new" component={NewPlace} exact />
+				<Redirect to="/" />
+			</Switch>
+    	</Router>
+  	)
 }
 
 export default App;
