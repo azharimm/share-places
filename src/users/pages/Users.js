3799b8d58947b5248069cecc9686685db8b2b175
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-04-10 09:41:59 +0700

    + users components

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
-const  Users = () => {
-  return <h2>Users Page</h2>
-}
+const Users = () => {
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Azhari',
+            image:
+                'https://www.greenscene.co.id/wp-content/uploads/2019/09/Zoro-One-Piece.jpg',
+            places: 3
+        }
+    ];
+    return <UsersList items={USERS} />;
+};
 
 export default Users;
