aa017ed9bd7400d221709087338891e64743b0a6
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-05-17 13:28:54 +0700

    ^ logout button

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { AuthContext } from '../../shared/context/auth-context';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
@@ -7,6 +8,7 @@ import Map from '../../shared/components/UIElements/Map';
 import './PlaceItem.css';
 
 const PlaceItem = (props) => {
+    const auth = useContext(AuthContext);
     const [showMap, setShowMap] = useState(false);
     const [showConfirmModal, setShowConfirmModal] = useState(false);
     const openMapHandler = () => setShowMap(true);
@@ -14,7 +16,7 @@ const PlaceItem = (props) => {
     const showDeleteWarningHandler = () => setShowConfirmModal(true);
     const hideDeleteWarningHandler = () => setShowConfirmModal(false);
     const confirmDeleteHandler = () => {
-        setShowConfirmModal(false)
+        setShowConfirmModal(false);
         console.log('deleted!');
     };
     return (
@@ -63,10 +65,14 @@ const PlaceItem = (props) => {
                         <Button onClick={openMapHandler} inverse>
                             VIEW ON MAP
                         </Button>
-                        <Button to={`/places/${props.id}`}>EDIT</Button>
-                        <Button danger onClick={showDeleteWarningHandler}>
-                            DELETE
-                        </Button>
+                        {auth.isLoggedIn && (
+                            <Button to={`/places/${props.id}`}>EDIT</Button>
+                        )}
+                        {auth.isLoggedIn && (
+                            <Button danger onClick={showDeleteWarningHandler}>
+                                DELETE
+                            </Button>
+                        )}
                     </div>
                 </Card>
             </li>
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -28,6 +28,11 @@ const NavLinks = (props) => {
                     <NavLink to="/auth">Authenticate</NavLink>
                 </li>
             )}
+            {auth.isLoggedIn && (
+                <li>
+                    <button onClick={auth.logout}>Logout</button>
+                </li>
+            )}
         </ul>
     );
 };
