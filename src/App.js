4b50340bd4d0dc2eecd04ffffbae79eb001b06a5
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-05-17 13:02:43 +0700

    + add auth context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {
     BrowserRouter as Router,
     Route,
     Redirect,
     Switch
 } from 'react-router-dom';
+import { AuthContext } from './shared/context/auth-context'
 import Users from './users/pages/Users';
 import Auth from './users/pages/Auth';
 import NewPlace from './places/pages/NewPlace';
@@ -13,20 +14,29 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const login = useCallback(() => {
+        setIsLoggedIn(true)
+    }, []);
+    const logout = useCallback(() => {
+        setIsLoggedIn(false)
+    }, []);
     return (
-        <Router>
-            <MainNavigation />
-            <main>
-                <Switch>
-                    <Route path="/" component={Users} exact />
-                    <Route path="/:userId/places" component={UserPlaces} exact />
-                    <Route path="/places/new" component={NewPlace} exact />
-                    <Route path="/places/:placeId" component={UpdatePlace} exact />
-                    <Route path="/auth" component={Auth} exact />
-                    <Redirect to="/" />
-                </Switch>
-            </main>
-        </Router>
+        <AuthContext.Provider value={isLoggedIn, login, logout}>
+            <Router>
+                <MainNavigation />
+                <main>
+                    <Switch>
+                        <Route path="/" component={Users} exact />
+                        <Route path="/:userId/places" component={UserPlaces} exact />
+                        <Route path="/places/new" component={NewPlace} exact />
+                        <Route path="/places/:placeId" component={UpdatePlace} exact />
+                        <Route path="/auth" component={Auth} exact />
+                        <Redirect to="/" />
+                    </Switch>
+                </main>
+            </Router>
+        </AuthContext.Provider>
     );
 };
 
