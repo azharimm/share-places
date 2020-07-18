8fa34578ad301a88f19530e671d6989eae9ac84f
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-07-18 07:18:13 +0700

    ^ error handler

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -71,6 +71,9 @@ const Auth = () => {
                     })
                 })
                 const data = await response.json();
+                if(!response.ok) {
+                    throw new Error(data.message)
+                }
                 console.log(data);
                 setIsLoading(false)
                 auth.login();
@@ -82,49 +85,54 @@ const Auth = () => {
         }
     };
 
+    const errorHandler = () => setError(null)
+
     return (
-        <Card className="authentication">
-            {isLoading && <LoadingSpinner asOverlay />}
-            <h2>Login Required</h2>
-            <hr />
-            <form onSubmit={authSubmitHandler}>
-                {!isLoginMode && (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorHandler} />
+            <Card className="authentication">
+                {isLoading && <LoadingSpinner asOverlay />}
+                <h2>Login Required</h2>
+                <hr />
+                <form onSubmit={authSubmitHandler}>
+                    {!isLoginMode && (
+                        <Input
+                            element="input"
+                            type="text"
+                            id="name"
+                            label="Your Name"
+                            validators={[VALIDATOR_REQUIRE]}
+                            errorText="Please enter your name."
+                            onInput={inputHandler}
+                        />
+                    )}
                     <Input
                         element="input"
-                        type="text"
-                        id="name"
-                        label="Your Name"
-                        validators={[VALIDATOR_REQUIRE]}
-                        errorText="Please enter your name."
+                        id="email"
+                        type="email"
+                        label="E-Email"
+                        validators={[VALIDATOR_EMAIL()]}
+                        errorText="Please enter a valid email!"
                         onInput={inputHandler}
                     />
-                )}
-                <Input
-                    element="input"
-                    id="email"
-                    type="email"
-                    label="E-Email"
-                    validators={[VALIDATOR_EMAIL()]}
-                    errorText="Please enter a valid email!"
-                    onInput={inputHandler}
-                />
-                <Input
-                    element="input"
-                    id="password"
-                    type="password"
-                    label="Password"
-                    validators={[VALIDATOR_MINLENGTH(5)]}
-                    errorText="Please enter a valid password at least 5 characters"
-                    onInput={inputHandler}
-                />
-                <Button type="submit" disabled={!formState.isValid}>
-                    {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+                    <Input
+                        element="input"
+                        id="password"
+                        type="password"
+                        label="Password"
+                        validators={[VALIDATOR_MINLENGTH(5)]}
+                        errorText="Please enter a valid password at least 5 characters"
+                        onInput={inputHandler}
+                    />
+                    <Button type="submit" disabled={!formState.isValid}>
+                        {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+                    </Button>
+                </form>
+                <Button inverse onClick={switchModeHandler}>
+                    SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
                 </Button>
-            </form>
-            <Button inverse onClick={switchModeHandler}>
-                SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
-            </Button>
-        </Card>
+            </Card>
+        </React.Fragment>
     );
 };
 
